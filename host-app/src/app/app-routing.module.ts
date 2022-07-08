02834f342843a117c4a515037f6afe9e9f8ba9ba
dav1t <dav1t.mam@gmail.com>
2022-07-08 22:29:47 +0400

    test

--- host-app/src/app/app-routing.module.ts
+++ host-app/src/app/app-routing.module.ts
@@ -5,15 +5,21 @@ import { MyComponentComponent } from './my-component/my-component.component';
 
 const routes: Routes = [
   {
-    path: '', component: LandingComponent
+    path: '',
+    component: LandingComponent,
   },
   {
-    path: 'my-component', component: MyComponentComponent
-  }
+    path: 'my-component',
+    component: MyComponentComponent,
+  },
+  {
+    path: 'ng-remote',
+    loadChildren: () => import('ngRemote/Module').then((m) => m.AppModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
